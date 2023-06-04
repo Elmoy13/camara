d8a117d174bb4ec2aaa3ea3ba5efd3eaca8d474b
Moises <moyses1234567890@gmail.com>
2023-06-03 20:49:03 -0600

    segundo commit

---
+++ android/app/src/main/java/com/camara/6H/MainActivity.java
@@ -0,0 +1,5 @@
+package com.camara.6H;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Camera, CameraResultType } from '@capacitor/camera';
+import { Geolocation, GeolocationPosition } from '@capacitor/geolocation';
 
 
 @Component({
@@ -9,6 +10,8 @@ import { Camera, CameraResultType } from '@capacitor/camera';
 })
 export class HomePage {
   capturedImage!: string;
+  latitude: number = 0;
+  longitude: number = 0;
   constructor() {}
 
   async takePhoto() {
@@ -27,7 +30,17 @@ export class HomePage {
   displayImage(imageData: string | undefined) {
     this.capturedImage = imageData ? 'data:image/jpeg;base64,' + imageData : '';
   }
-
+  async getCurrentLocation() {
+    try {
+      const position: GeolocationPosition = await Geolocation.getCurrentPosition();
+      this.latitude = position.coords.latitude;
+      this.longitude = position.coords.longitude;
+      console.log('Latitud: ', this.latitude);
+      console.log('Longitud: ', this.longitude);
+    } catch (error) {
+      console.error('Error al obtener la ubicación', error);
+    }
+  }
 
 
 }
